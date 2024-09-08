c372a9284022df7ed19d7163a2a48f46cff0a46a
Shreyyy5 <120664375+Shreyyy5@users.noreply.github.com>
2024-09-08 22:34:50 +0530

    Update appear.component.ts

--- src/app/appear/appear.component.ts
+++ src/app/appear/appear.component.ts
@@ -9,11 +9,24 @@ export class AppearComponent {
   view = 'aiNav'; // default view
   isAiNav = true; // Default to AI Nav view
   toggleText = 'AI Nav';
-
+  businessName: string = '';
+  agentName: string = '';
 
   toggleView(view: string) {
     this.view = view;
   }
+
+  // tips
+    // tip popup
+    showTipsPopup = false;
+
+    openTipsPopup() {
+      this.showTipsPopup = true;  // Show the popup
+    }
+  
+    closeTipsPopup() {
+      this.showTipsPopup = false;  // Hide the popup
+    }
 
   // suggested questions
   questions = [
@@ -50,4 +63,95 @@ export class AppearComponent {
   selectColor(color: string): void {
     this.selectedColor = color;
   }
+
+  // avatar
+  uploadAvatar() {
+    document.getElementById('avatarInput').click();
+  }
+  
+  onAvatarSelected(event) {
+    const file = event.target.files[0];
+    // You can then upload the file to your server or use it as needed
+    console.log(file);
+  }
+
+  toggleState: boolean = false;
+
+  // conversations
+  actions: any[] = [];
+  actionOptions: string[] = [
+    'Website Link',
+    'Email',
+    'Phone'
+  ];
+  isDropdownOpen: boolean = false;
+
+  addAction() {
+    this.actions.push({ action: '', url: '' });
+  }
+
+
+  removeAction(index: number) {
+    this.actions.splice(index, 1);
+  }
+
+
+
+  // upload popup
+showPopup = false;
+
+addUpload() {
+  this.showPopup = true; // Show the popup when the button is clicked
+}
+
+closePopup() {
+  this.showPopup = false; // Close the popup
+}
+
+onFileSelected(event: Event) {
+  const fileInput = event.target as HTMLInputElement;
+  if (fileInput.files && fileInput.files.length > 0) {
+    const file = fileInput.files[0];
+    console.log('Selected file:', file);
+    // Handle file upload logic here
+  }
+  
+}
+// add actionables
+isDropdownVisible = false;
+customActions = [];
+
+toggleDropdown() {
+  this.isDropdownVisible = !this.isDropdownVisible;
+}
+
+addCustomAction(type: string) {
+  this.customActions.push({ title: '', value: '', type });
+  this.isDropdownVisible = false;
+}
+
+getCustomPlaceholder(type: string): string {
+  switch (type) {
+    case 'Website Link':
+      return 'https://...';
+    case 'Email':
+      return 'email@example.com';
+    case 'Phone':
+      return '+123456789';
+    default:
+      return '';
+  }
+}
+
+addCustomRow(index: number) {
+  this.customActions.splice(index + 1, 0, { title: '', value: '', type: this.customActions[index].type });
+}
+
+removeCustomRow(index: number) {
+  this.customActions.splice(index, 1);
+}
+
+// cust
+
+
 }
