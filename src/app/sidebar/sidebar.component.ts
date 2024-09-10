82cc41e94629f474fecc291e2ada906cd46f66ec
Shreyyy5 <120664375+Shreyyy5@users.noreply.github.com>
2024-09-10 15:27:08 +0530

    Update sidebar.component.ts

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  languages: string[] = [
+    'Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani',
+    'Basque', 'Belarusian', 'Bengali', 'Bosnian', 'Bulgarian', 'Catalan',
+    'Cebuano', 'Chichewa', 'Chinese', 'Danish',
+    'Dutch', 'English',  'French',
+    'Frisian', 'Galician', 'Georgian', 'German', 'Greek', 'Gujarati', 'Haitian Creole',
+     'Hindi', 'Hmong', 'Hungarian', 'Icelandic', 'Igbo',
+    'Indonesian', 'Irish', 'Italian', 'Japanese', 'Javanese', 'Kannada', 'Kazakh',
+     'Latin', 'Latvian',
+     'Malayalam', 'Maltese',
+    'Maori', 'Marathi', 'Mongolian', 'Myanmar (Burmese)', 'Nepali', 'Norwegian', 'Odia (Oriya)',
+    'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Romanian', 'Russian', 'Samoan',
+     'Swahili', 'Swedish', 'Tajik', 'Tamil', 'Tatar', 'Telugu',
+    'Thai', 'Turkish', 'Turkmen', 'Ukrainian', 'Urdu', 
+    'Xhosa', 'Yiddish', 'Yoruba', 'Zulu'
+  ];
+  languagesInRows: string[][] = [];
 
+  constructor() {
+    this.initializeLanguageGrid();
+  }
+
+  initializeLanguageGrid() {
+    let rowIndex = 0;
+    for (let i = 0; i < this.languages.length; i += 9) {
+      this.languagesInRows[rowIndex] = this.languages.slice(i, i + 9);
+      rowIndex++;
+    }
+  }
+
+  openLanguageModal() {
+    const modal = document.getElementById('languageModal');
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  }
+
+  closeLanguageModal() {
+    const modal = document.getElementById('languageModal');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  }
 }
